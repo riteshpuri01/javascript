a395e6d4374f703da43f43318148eba476f6fbca
riteshpuri01 <riteshpuri789@gmail.com>
2025-11-17 19:49:34 +0530

    commit

--- 01_Basics.js/02_datatype.js
+++ 01_Basics.js/02_datatype.js
@@ -1,3 +1,4 @@
+// Primitive DT
 let num = 1
 let string = "Ritesh"
 let boolean = true //or false
@@ -5,3 +6,20 @@ let undefined; // means no value is defined in the variable
 let nullvalue = null // means it is a object
 
 console.log(num, string, boolean, undefined, nullvalue);
+
+// Non-Primitive DT : Array, Objetcs, Functions
+array = ["1, 2, 3"]; // Array
+let object = {  // Variable 
+    name: "ritesh",  // Object within bracket 
+    age: 24
+}
+//Proper definition of Function: function(){} 
+const myfunction = function(){ //function in a variable 
+    console.log("Hello world");
+}
+
+//Backtick : Better to write for an output 
+const name = "Ritesh"
+const age = 24
+
+console.log(`My name is ${name} and my age is ${age}`);
---
+++ 01_Basics.js/04_opreators.js
@@ -0,0 +1,10 @@
+// console.log(2 + 2); Add
+// console.log(2 - 2); Sub
+// console.log(2 * 2); Multiply
+// console.log(2 / 2); Divide
+// console.log(2 % 2); Remainder
+
+console.log(1 + "1"); 
+console.log("1" + 1);
+console.log(1 + 1 + "1");
+console.log("1" + 1 + 1);
